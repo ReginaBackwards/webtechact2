ca5d46b0143496734cf87c48e90f27fbd4b20b50
Abby Gaile Lacorte <2220908@slu.edu.ph>
2023-10-19 04:32:32 +0800

    update

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-
 mapboxgl.accessToken = 'pk.eyJ1IjoicmVnaW5hYmFja3dhcmRzIiwiYSI6ImNsbnN4Y3JqcDFoM2YybW8ycTd2ZGJqZjQifQ.uKv4TTT2HDybR6e_l8UDdg';
+
 var map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/mapbox/streets-v11'
 });
+
 map.addControl(new mapboxgl.NavigationControl());
+
+function search() {
+  const from = document.getElementById("from").value;
+  const to = document.getElementById("to").value;
+}
