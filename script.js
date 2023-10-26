9babfdff9ec1f840774e98b0c6e7e31f409f739a
paul ivan dimacali <2223496@slu.edu.ph>
2023-10-26 23:37:34 +0800

    oh may linya na sa mapa pano ba yan

--- script.js
+++ script.js
@@ -10,7 +10,77 @@ var map = new mapboxgl.Map({
   style: 'mapbox://styles/mapbox/streets-v11'
 });
 
+const draw = new MapboxDraw({
+  // Instead of showing all the draw tools, show only the line string and delete tools.
+  displayControlsDefault: false,
+  controls: {
+    line_string: true,
+    trash: true
+  },
+  // Set the draw mode to draw LineStrings by default.
+  defaultMode: 'draw_line_string',
+  styles: [
+    // Set the line style for the user-input coordinates.
+    {
+      id: 'gl-draw-line',
+      type: 'line',
+      filter: ['all', ['==', '$type', 'LineString'], ['!=', 'mode', 'static']],
+      layout: {
+        'line-cap': 'round',
+        'line-join': 'round'
+      },
+      paint: {
+        'line-color': '#438EE4',
+        'line-dasharray': [0.2, 2],
+        'line-width': 4,
+        'line-opacity': 0.7
+      }
+    },
+    // Style the vertex point halos.
+    {
+      id: 'gl-draw-polygon-and-line-vertex-halo-active',
+      type: 'circle',
+      filter: [
+        'all',
+        ['==', 'meta', 'vertex'],
+        ['==', '$type', 'Point'],
+        ['!=', 'mode', 'static']
+      ],
+      paint: {
+        'circle-radius': 12,
+        'circle-color': '#FFF'
+      }
+    },
+    // Style the vertex points.
+    {
+      id: 'gl-draw-polygon-and-line-vertex-active',
+      type: 'circle',
+      filter: [
+        'all',
+        ['==', 'meta', 'vertex'],
+        ['==', '$type', 'Point'],
+        ['!=', 'mode', 'static']
+      ],
+      paint: {
+        'circle-radius': 8,
+        'circle-color': '#438EE4'
+      }
+    }
+  ]
+});
+
+function removeRoute() {
+  if (!map.getSource('route')) return;
+  map.removeLayer('route');
+  map.removeSource('route');
+}
+
+
+map.on('draw.delete', removeRoute);
 map.addControl(new mapboxgl.NavigationControl());
+map.addControl(draw);
+
+
 
 button.addEventListener('click', place);
 
