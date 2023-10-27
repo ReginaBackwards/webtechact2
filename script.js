9df755cffacaf91136cc8f69d16aeec39746fd4e
America Eloise Slay <2220163@slu.edu.ph>
2023-10-28 03:10:08 +0800

    ROR

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoicmVnaW5hYmFja3dhcmRzIiwiYSI6ImNsbnN4Y3JqcDFoM2YybW8ycTd2ZGJqZjQifQ.uKv4TTT2HDybR6e_l8UDdg';
 const philippinesBounds = {
-    latMin: 4.5,
-    latMax: 21.5,
-    lonMin: 115.0,
-    lonMax: 128.0,
+  latMin: 4.5,
+  latMax: 21.5,
+  lonMin: 115.0,
+  lonMax: 128.0,
 };
 
 const map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v11',
-    center: [121, 13],
-    zoom: 7,
-    maxBounds: [
-        [philippinesBounds.lonMin, philippinesBounds.latMin],
-        [philippinesBounds.lonMax, philippinesBounds.latMax]
-    ],
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v11',
+  center: [121, 13],
+  zoom: 7,
+  maxBounds: [
+    [philippinesBounds.lonMin, philippinesBounds.latMin],
+    [philippinesBounds.lonMax, philippinesBounds.latMax]
+  ],
 });
 
 let currentMarkers = [];
@@ -22,109 +22,107 @@ let airports = [];
 
 // Function to create an airport button
 function createAirportButton(airport) {
-    const airportList = document.getElementById('airportData');
+  const airportList = document.getElementById('airportData');
 
-    const airportButton = document.createElement('button');
-    airportButton.innerHTML = `${airport.name}<br>${airport.iata}`;
-    airportButton.addEventListener('click', () => {
-        // Zoom in on the location of an airport when clicked
-        map.flyTo({
-            center: [airport.lon, airport.lat],
-            zoom: 14,
-        });
+  const airportButton = document.createElement('button');
+  airportButton.innerHTML = `${airport.name}<br>${airport.iata}`;
+  airportButton.addEventListener('click', () => {
+    // Zoom in on the location of an airport when clicked
+    map.flyTo({
+      center: [airport.lon, airport.lat],
+      zoom: 14,
     });
+  });
 
-    airportList.appendChild(airportButton);
+  airportList.appendChild(airportButton);
 }
-
-
 
 // Function to add a marker for an airport
 function addAirportMarker(airport) {
-    const marker = new mapboxgl.Marker()
-        .setLngLat([airport.lon, airport.lat])
-        .setPopup(new mapboxgl.Popup().setHTML(`<h3>${airport.name}</h3><p>IATA: ${airport.iata}</p><p>Latitude: ${airport.lat}</p><p>Longitude: ${airport.lon}</p>`))
-        .addTo(map);
-    return marker;
+  const marker = new mapboxgl.Marker()
+    .setLngLat([airport.lon, airport.lat])
+    .setPopup(new mapboxgl.Popup().setHTML(`<h3>${airport.name}</h3><p>IATA: ${airport.iata}</p><p>Latitude: ${airport.lat}</p><p>Longitude: ${airport.lon}</p>`))
+    .addTo(map);
+  return marker;
 }
 
 // Function to create markers for all airports
 function createMarkersForAllAirports() {
-    airports.forEach(airport => {
-        const marker = addAirportMarker(airport);
-        currentMarkers.push(marker);
-    });
+  airports.forEach(airport => {
+    const marker = addAirportMarker(airport);
+    currentMarkers.push(marker);
+  });
 }
 
 // Function to handle search input changes
 function handleSearchInputChange() {
-    const searchInput = document.getElementById('airportSearch');
-    const searchQuery = searchInput.value.toLowerCase();
+  const searchInput = document.getElementById('airportSearch');
+  const searchQuery = searchInput.value.toLowerCase();
 
-    // Filter airports based on the search query
-    const filteredAirports = airports.filter(airport => airport.name.toLowerCase().includes(searchQuery));
+  // Filter airports based on the search query
+  const filteredAirports = airports.filter(airport => airport.name.toLowerCase().includes(searchQuery));
 
-    // Clear existing markers and airport buttons
-    clearMarkers();
-    clearAirportButtons();
+  // Clear existing markers and airport buttons
+  clearMarkers();
+  clearAirportButtons();
 
-    // Create markers for filtered airports and buttons for the list
-    createMarkersForFilteredAirports(filteredAirports);
-    createButtonsForFilteredAirports(filteredAirports);
+  // Create markers for filtered airports and buttons for the list
+  createMarkersForFilteredAirports(filteredAirports);
+  createButtonsForFilteredAirports(filteredAirports);
 }
 
 // Function to clear all markers
 function clearMarkers() {
-    currentMarkers.forEach(marker => marker.remove());
-    currentMarkers = [];
+  currentMarkers.forEach(marker => marker.remove());
+  currentMarkers = [];
 }
 
 // Function to create markers for filtered airports
 function createMarkersForFilteredAirports(filteredAirports) {
-    filteredAirports.forEach(airport => {
-        const marker = addAirportMarker(airport);
-        currentMarkers.push(marker);
-    });
+  filteredAirports.forEach(airport => {
+    const marker = addAirportMarker(airport);
+    currentMarkers.push(marker);
+  });
 }
 
 // Function to clear all airport buttons
 function clearAirportButtons() {
-    const airportList = document.getElementById('airportData');
-    while (airportList.firstChild) {
-        airportList.removeChild(airportList.firstChild);
-    }
+  const airportList = document.getElementById('airportData');
+  while (airportList.firstChild) {
+    airportList.removeChild(airportList.firstChild);
+  }
 }
 
 // Function to create buttons for filtered airports
 function createButtonsForFilteredAirports(filteredAirports) {
-    const airportList = document.getElementById('airportData');
-    filteredAirports.forEach(airport => {
-        createAirportButton(airport);
-    });
+  const airportList = document.getElementById('airportData');
+  filteredAirports.forEach(airport => {
+    createAirportButton(airport);
+  });
 }
 
 // Fetch airport data and create buttons and markers
 function fetchAirportData() {
-    fetch('https://flight-radar1.p.rapidapi.com/airports/list', {
-        headers: {
-            'X-RapidAPI-Key': '3abd92b282msh2a1fd5ea65acf66p146728jsnb1a0fb7690fd',
-        },
-    })
-        .then(response => response.json())
-        .then(data => {
-            if (data.rows) {
-                airports = data.rows.filter(
-                    airport => airport.lat && airport.lon && airport.country === 'Philippines'
-                );
+  fetch('https://flight-radar1.p.rapidapi.com/airports/list', {
+    headers: {
+      'X-RapidAPI-Key': '3abd92b282msh2a1fd5ea65acf66p146728jsnb1a0fb7690fd',
+    },
+  })
+    .then(response => response.json())
+    .then(data => {
+      if (data.rows) {
+        airports = data.rows.filter(
+          airport => airport.lat && airport.lon && airport.country === 'Philippines'
+        );
 
-                // Create buttons and markers for all airports
-                createButtonsForFilteredAirports(airports);
-                createMarkersForAllAirports();
-            } else {
-                console.error('API response does not contain airport data:', data);
-            }
-        })
-        .catch(error => console.error(error));
+        // Create buttons and markers for all airports
+        createButtonsForFilteredAirports(airports);
+        createMarkersForAllAirports();
+      } else {
+        console.error('API response does not contain airport data:', data);
+      }
+    })
+    .catch(error => console.error(error));
 }
 
 // Add event listener to search input
@@ -137,8 +135,8 @@ function findNearestAirportAndDrawLine(coordinates) {
   const nearestAirport = findNearestAirport(coordinates);
 
   if (!nearestAirport) {
-      console.error('No airports found');
-      return;
+    console.error('No airports found');
+    return;
   }
 
   clearAirportButtons();
@@ -157,14 +155,14 @@ function calculateDistance(coord1, coord2) {
   const lon1 = coord1[0];
   const lat2 = coord2[1];
   const lon2 = coord2[0];
-  
+
   const R = 6371; // Radius of the Earth in kilometers
   const dLat = (lat2 - lat1) * (Math.PI / 180);
   const dLon = (lon2 - lon1) * (Math.PI / 180);
   const a =
-      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-      Math.cos(lat1 * (Math.PI / 180)) * Math.cos(lat2 * (Math.PI / 180)) *
-      Math.sin(dLon / 2) * Math.sin(dLon / 2);
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(lat1 * (Math.PI / 180)) * Math.cos(lat2 * (Math.PI / 180)) *
+    Math.sin(dLon / 2) * Math.sin(dLon / 2);
   const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
   const distance = R * c;
 
@@ -175,9 +173,9 @@ function calculateDistance(coord1, coord2) {
 function findNearestAirport(coordinates) {
   const sortedAirports = airports.slice(); // Copy the airports array
   sortedAirports.sort((a, b) => {
-      const distanceA = calculateDistance(coordinates, [a.lon, a.lat]);
-      const distanceB = calculateDistance(coordinates, [b.lon, b.lat]);
-      return distanceA - distanceB;
+    const distanceA = calculateDistance(coordinates, [a.lon, a.lat]);
+    const distanceB = calculateDistance(coordinates, [b.lon, b.lat]);
+    return distanceA - distanceB;
   });
 
   return sortedAirports[0];
@@ -186,7 +184,7 @@ function findNearestAirport(coordinates) {
 // Function to draw a line to an airport
 function drawLineToAirport(fromCoordinates, airport) {
   const lineId = `lineToAirport${airport.iata}`; // Use a unique identifier based on the airport IATA code
-  
+
   // Check if the source exists and remove it
   if (map.getSource(lineId)) {
     map.removeSource(lineId);
@@ -265,7 +263,7 @@ function removeLine() {
 function clearNearestAirportInfo() {
   const nearestAirportInfo = document.getElementById('nearestAirportInfo');
   while (nearestAirportInfo.firstChild) {
-      nearestAirportInfo.removeChild(nearestAirportInfo.firstChild);
+    nearestAirportInfo.removeChild(nearestAirportInfo.firstChild);
   }
 }
 
