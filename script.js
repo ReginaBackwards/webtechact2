fb0b93c897a48912e327e1ea66c046ce2f8dc2bf
JANBERT DELA CRUZ <2220836@slu.edu.ph>
2023-10-27 20:05:42 +0800

    buttons buttoning

--- script.js
+++ script.js
@@ -1,27 +1,58 @@
-// Replace with your Mapbox Access Token
 mapboxgl.accessToken = 'pk.eyJ1IjoicmVnaW5hYmFja3dhcmRzIiwiYSI6ImNsbnN4Y3JqcDFoM2YybW8ycTd2ZGJqZjQifQ.uKv4TTT2HDybR6e_l8UDdg';
 
-// Define the boundaries for the Philippines
 const philippinesBounds = {
-    latMin: 4.5, // Adjust as needed (lower latitude)
-    latMax: 21.5, // Adjust as needed (higher latitude)
-    lonMin: 115.0, // Adjust as needed
-    lonMax: 128.0, // Adjust as needed
+    latMin: 4.5,
+    latMax: 21.5,
+    lonMin: 115.0,
+    lonMax: 128.0,
 };
 
-// Create a map with the adjusted view
-var map = new mapboxgl.Map({
+const map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v11', // Mapbox style URL
-    center: [121, 13], // Center the map on the Philippines (longitude, latitude)
-    zoom: 7, // Set the initial zoom level for the Philippines
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [121, 13],
+    zoom: 7,
     maxBounds: [
         [philippinesBounds.lonMin, philippinesBounds.latMin],
         [philippinesBounds.lonMax, philippinesBounds.latMax]
     ],
 });
 
-// Function to fetch airport data and display markers on the map
+let currentMarker;
+
+function createAirportButton(airport) {
+    const airportList = document.getElementById('airportData');
+
+    const airportButton = document.createElement('button');
+    airportButton.textContent = `${airport.name} - ${airport.iata}`;
+    airportButton.addEventListener('click', () => {
+        // When an airport button is clicked, zoom in on the airport's location
+        map.flyTo({
+            center: [airport.lon, airport.lat],
+            zoom: 12,
+        });
+
+        // Remove the previous marker
+        if (currentMarker) {
+            currentMarker.remove();
+        }
+
+        // Add a marker for the selected airport
+        currentMarker = addAirportMarker(airport);
+    });
+
+    airportList.appendChild(airportButton);
+}
+
+function addAirportMarker(airport) {
+    const marker = new mapboxgl.Marker()
+        .setLngLat([airport.lon, airport.lat])
+        .setPopup(new mapboxgl.Popup().setHTML(`<h3>${airport.name}</h3><p>${airport.iata}</p>`))
+        .addTo(map);
+
+    return marker;
+}
+
 function fetchAirportData() {
     fetch('https://flight-radar1.p.rapidapi.com/airports/list', {
         headers: {
@@ -31,33 +62,12 @@ function fetchAirportData() {
         .then(response => response.json())
         .then(data => {
             if (data.rows) {
-                const airportList = document.getElementById('airportData');
-
                 data.rows.forEach(row => {
                     const airport = row;
                     if (airport.lat && airport.lon && airport.country === 'Philippines') {
-                        // Create a list item for each airport in the Philippines
-                        const listItem = document.createElement('li');
-                        listItem.textContent = `${airport.name} - ${airport.iata}`;
-                        listItem.addEventListener('click', () => {
-                            // When a list item representing an airport is clicked, display a marker on the map
-                            new mapboxgl.Marker()
-                                .setLngLat([airport.lon, airport.lat])
-                                .setPopup(new mapboxgl.Popup().setHTML(`<h3>${airport.name}</h3><p>${airport.iata}</p>`))
-                                .addTo(map);
-                        });
-                        airportList.appendChild(listItem);
-                    }
-                });
-
-                // Add markers to the map for Philippine airports
-                data.rows.forEach(row => {
-                    const airport = row;
-                    if (airport.lat && airport.lon && airport.country === 'Philippines') {
-                        new mapboxgl.Marker()
-                            .setLngLat([airport.lon, airport.lat])
-                            .setPopup(new mapboxgl.Popup().setHTML(`<h3>${airport.name}</h3><p>${airport.iata}</p>`))
-                            .addTo(map);
+                        createAirportButton(airport);
+                        // Initially, add markers for all airports
+                        addAirportMarker(airport);
                     }
                 });
             } else {
@@ -67,5 +77,4 @@ function fetchAirportData() {
         .catch(error => console.error(error));
 }
 
-// Call the fetchAirportData function to load and display markers for Philippine airports
 fetchAirportData();
