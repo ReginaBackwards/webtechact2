d86e9f8a8660a0b6b51b634684ea3b59acfe7358
JANBERT DELA CRUZ <2220836@slu.edu.ph>
2023-10-28 00:03:49 +0800

    ME N MIMI PIX DA WORLD

--- script.js
+++ script.js
@@ -131,3 +131,216 @@ const searchInput = document.getElementById('airportSearch');
 searchInput.addEventListener('input', handleSearchInputChange);
 
 fetchAirportData();
+
+// ... (previous code)
+
+// Update the findNearestAirportAndDrawLine function
+function findNearestAirportAndDrawLine(coordinates) {
+  const nearestAirport = findNearestAirport(coordinates);
+
+  if (!nearestAirport) {
+      console.error('No airports found');
+      return;
+  }
+
+  clearAirportButtons();
+
+  // Draw a line to the nearest airport and show its distance
+  drawLineToAirport(coordinates, nearestAirport);
+  showDistanceToAirport(coordinates, nearestAirport);
+
+  // Create a button for the nearest airport
+  createAirportButton(nearestAirport);
+}
+
+
+// Function to calculate the distance between two sets of coordinates
+function calculateDistance(coord1, coord2) {
+  const lat1 = coord1[1];
+  const lon1 = coord1[0];
+  const lat2 = coord2[1];
+  const lon2 = coord2[0];
+  
+  const R = 6371; // Radius of the Earth in kilometers
+  const dLat = (lat2 - lat1) * (Math.PI / 180);
+  const dLon = (lon2 - lon1) * (Math.PI / 180);
+  const a =
+      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.cos(lat1 * (Math.PI / 180)) * Math.cos(lat2 * (Math.PI / 180)) *
+      Math.sin(dLon / 2) * Math.sin(dLon / 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  const distance = R * c;
+
+  return distance;
+}
+
+// Function to find the nearest airports
+function findNearestAirport(coordinates) {
+  const sortedAirports = airports.slice(); // Copy the airports array
+  sortedAirports.sort((a, b) => {
+      const distanceA = calculateDistance(coordinates, [a.lon, a.lat]);
+      const distanceB = calculateDistance(coordinates, [b.lon, b.lat]);
+      return distanceA - distanceB;
+  });
+
+  return sortedAirports[0];
+}
+
+
+// Function to draw a line to an airport
+function drawLineToAirport(fromCoordinates, airport) {
+  const lineId = `lineToAirport${airport.iata}`; // Use a unique identifier based on the airport IATA code
+  
+  // Check if the source exists and remove it
+  if (map.getSource(lineId)) {
+    map.removeSource(lineId);
+  }
+
+  // Check if the layer exists and remove it
+  if (map.getLayer(lineId)) {
+    map.removeLayer(lineId);
+  }
+
+  const lineGeoJSON = {
+    type: 'Feature',
+    geometry: {
+      type: 'LineString',
+      coordinates: [fromCoordinates, [airport.lon, airport.lat]],
+    },
+  };
+
+  map.addSource(lineId, {
+    type: 'geojson',
+    data: lineGeoJSON,
+  });
+
+  map.addLayer({
+    id: lineId,
+    type: 'line',
+    source: lineId,
+    layout: {
+      'line-join': 'round',
+      'line-cap': 'round',
+    },
+    paint: {
+      'line-color': 'blue',
+      'line-width': 3,
+    },
+  });
+}
+
+// Function to show the distance to an airport
+function showDistanceToAirport(fromCoordinates, airport) {
+  const distance = calculateDistance(fromCoordinates, [airport.lon, airport.lat]);
+  const formattedDistance = distance.toFixed(2) + ' km';
+
+  const nearestAirportInfo = document.getElementById('nearestAirportInfo');
+
+  // Create a new paragraph element for the nearest airport
+  const infoElement = document.createElement('div');
+  // infoElement.textContent = `${airport.name} - Distance: ${formattedDistance}`;
+
+  infoElement.innerHTML = `<h2>${airport.name} - ${airport.iata}</h2>
+    <h3>Distance: ${formattedDistance}</h3>
+    <p>City: ${airport.city}</p>
+    <p>Latitude: ${airport.lat}</p>
+    <p>Longitude: ${airport.lon}</p>
+    <p>Country: ${airport.country}</p>
+    <p>Altitude: ${airport.alt} meters</p>
+    <p>Timezone: ${airport.timezone.name} (${airport.timezone.abbr})</p>`;
+
+  nearestAirportInfo.appendChild(infoElement);
+}
+
+let redMarker = null;
+let previousNearestAirport = null;
+let lineId = null;
+
+// Function to remove the line
+function removeLine() {
+  if (lineId && map.getLayer(lineId)) {
+    map.removeLayer(lineId);
+  }
+  if (lineId && map.getSource(lineId)) {
+    map.removeSource(lineId);
+  }
+}
+
+// Function to clear the nearest airport information
+function clearNearestAirportInfo() {
+  const nearestAirportInfo = document.getElementById('nearestAirportInfo');
+  while (nearestAirportInfo.firstChild) {
+      nearestAirportInfo.removeChild(nearestAirportInfo.firstChild);
+  }
+}
+
+map.on('click', (e) => {
+  const coordinates = e.lngLat.toArray();
+
+  // Clear previous nearest airport info and red marker
+  clearNearestAirportInfo();
+  if (redMarker) {
+    redMarker.remove();
+  }
+
+  // Remove the previous line
+  removeLine();
+
+  // Add a red marker at the clicked location
+  redMarker = new mapboxgl.Marker({ color: 'red' })
+    .setLngLat(coordinates)
+    .addTo(map);
+
+  // Create a unique line ID based on the current timestamp
+  lineId = `lineToAirport${Date.now()}`;
+
+  findNearestAirportAndDrawLine(coordinates);
+});
+
+// Function to draw a line to an airport
+function drawLineToAirport(fromCoordinates, airport) {
+  // Check if the source and layer with the same ID exist and remove them
+  if (map.getSource(lineId)) {
+    map.removeSource(lineId);
+  }
+  if (map.getLayer(lineId)) {
+    map.removeLayer(lineId);
+  }
+
+  const lineGeoJSON = {
+    type: 'Feature',
+    geometry: {
+      type: 'LineString',
+      coordinates: [fromCoordinates, [airport.lon, airport.lat]],
+    },
+  };
+
+  map.addSource(lineId, {
+    type: 'geojson',
+    data: lineGeoJSON,
+  });
+
+  map.addLayer({
+    id: lineId,
+    type: 'line',
+    source: lineId,
+    layout: {
+      'line-join': 'round',
+      'line-cap': 'round',
+    },
+    paint: {
+      'line-color': 'blue',
+      'line-width': 3,
+    },
+  });
+}
+
+// Function to reset the map
+function resetMap() {
+  // Reload the page
+  location.reload();
+}
+
+const resetButton = document.getElementById('resetMap');
+resetButton.addEventListener('click', resetMap);
+
